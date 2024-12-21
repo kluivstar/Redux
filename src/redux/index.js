0493bd699fb3197ef612285cce42a56bd2f1966b
Dev Denzel <100281304+devdenzel@users.noreply.github.com>
2024-12-21 04:25:32 +0100

    Created multiple Reducers and States, allowing each Action to handle specific task

--- src/redux/index.js
+++ src/redux/index.js
@@ -2,8 +2,9 @@ const redux = require('redux')
 const createStore = redux.createStore // create store function
 
 const BUY_CAKE ='BUY_CAKE'
+const BUY_ICECREAM = 'BUY_ICECREAM'
 
-// Action
+// Action or one shop keeper
 function buyCake(){
     return {
         type: BUY_CAKE,
@@ -12,25 +13,75 @@ function buyCake(){
     
 }
 
-// State
-const initialState = {
+function buyIceCreams(){
+    return {
+        type: BUY_ICECREAM,
+        info: '' // optional metadata about action
+    }
+    
+}
+
+// Old State
+// const initialState = {
+//     numOfCakes: 10,
+//     numOfIceCreams: 20
+// }
+
+// Split States
+const initialCakeState = {
     numOfCakes: 10
 }
 
-// Reducer
-const reducer = (state = initialState, action) => {
-    switch(action.type) {
-        case BUY_CAKE: return {
-            ... state, // Copy of state
-            numOfCakes: state.numOfCakes -1
-        }
-        default: return state
-    }
+const initialIceCreamState = {
+    numOfIceCreams: 20
 }
 
+// Old Reducer
+// const reducer = (state = initialState, action) => {
+//     switch(action.type) {
+//         case BUY_CAKE: return {
+//             ... state, // Copy of state
+//             numOfCakes: state.numOfCakes -1
+//         }
+//         case BUY_ICECREAM: return {
+//             ... state, // Copy of state
+//             numOfIceCreams: state.numOfIceCreams -1
+//         }
+//         default: return state
+//     }
+// }
+
+// New Reducers handling individual actions
+const cakeReducer = (state= initialCakeState, action) => {
+    switch(action.type) {
+                case BUY_CAKE: return {
+                    ... state, // Copy of state
+                    numOfCakes: state.numOfCakes -1
+                }
+                default: return state
+            }
+}
+
+const iceCreamReducer = (state= initialIceCreamState, action) => {
+    switch(action.type) {
+                case BUY_ICECREAM: return {
+                    ... state, // Copy of state
+                    numOfIceCreams: state.numOfIceCreams -1
+                }
+                default: return state
+            }
+}
+
+// Create Store
 const store = createStore(reducer)
 console.log('Initial state', store.getState())
+
 const unsubscribe = store.subscribe(() => console.log('Updated state', store.getState()))
+
+// Dispatch actions
 store.dispatch(buyCake())
 store.dispatch(buyCake())
+store.dispatch(buyIceCreams())
+
+// Unsubcribe
 unsubscribe()
