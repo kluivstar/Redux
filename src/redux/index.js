d216b029e09e89d0ce543b2fe0d57aab9a417370
Dev Denzel <100281304+devdenzel@users.noreply.github.com>
2024-12-21 03:45:25 +0100

    Created a store and listened for state changes using the 'subscribe' method

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,10 +1,13 @@
+const redux = require('redux')
+const createStore = redux.createStore // create store function
+
 const BUY_CAKE ='BUY_CAKE'
 
 // Action
 function buyCake(){
     return {
         type: BUY_CAKE,
-        info: 'First redux action'
+        info: 'First redux action' // optional metadata about action
     }
     
 }
@@ -24,3 +27,10 @@ const reducer = (state = initialState, action) => {
         default: return state
     }
 }
+
+const store = createStore(reducer)
+console.log('Initial state', store.getState())
+const unsubscribe = store.subscribe(() => console.log('Updated state', store.getState()))
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+unsubscribe()
