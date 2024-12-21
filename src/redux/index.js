8a1d3b865ec9d9b6db181633042a94c5dfc9a610
Dev Denzel <100281304+devdenzel@users.noreply.github.com>
2024-12-21 02:31:20 +0100

    Implement Redux basic: actions, reducers and state management. Includes an actions to 'buy a cake' and reducer to handle state change

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,9 +1,26 @@
 const BUY_CAKE ='BUY_CAKE'
 
+// Action
 function buyCake(){
     return {
         type: BUY_CAKE,
         info: 'First redux action'
     }
     
+}
+
+// State
+const initialState = {
+    numOfCakes: 10
+}
+
+// Reducer
+const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        case BUY_CAKE: return {
+            ... state, // Copy of state
+            numOfCakes: state.numOfCakes -1
+        }
+        default: return state
+    }
 }
