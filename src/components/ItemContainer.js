a1244c6d894945cd7ab06b9f5c56640de2beeab5
Dev Denzel <100281304+devdenzel@users.noreply.github.com>
2024-12-27 03:06:42 +0100

    Added mapDispatchToProps to dispatch actions based on props received, it binds dispatch to components props

--- src/components/ItemContainer.js
+++ src/components/ItemContainer.js
@@ -1,20 +1,40 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { buyCake, buyIceCream } from '../redux'
 
 const ItemContainer = (props) => {
   return (
-    <h1>
+   <div class="">
+     <h1>
         Item - {props.item}
     </h1>
+    <button type="" class="" onClick={props.buyItem}>Buy Item</button>
+   </div>
   )
 }
 
 const mapStateToProps = (state, ownProps) => {
+  // binds/maps state to component prop if prop(cake) is passed
+  
     const itemState = ownProps.cake
+    // if cake is passed number of cake is returned
     ? state.cake.numOfCakes
+    // else
     : state.iceCream.numOfIceCreams
     return {
         item: itemState
     }
 }
-export default connect(mapStateToProps)(ItemContainer)
+
+const mapDispatchToProps = (dispatch, ownProps) =>{
+  const dispatchFunction = ownProps.cake
+  // maps Redux dispatch actions to the component's props.
+  // buyItem is now avaliable as a prop to this component
+  ? () => dispatch(buyCake())
+  : () => dispatch(buyIceCream())
+  return {
+    buyItem: dispatchFunction
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ItemContainer)
